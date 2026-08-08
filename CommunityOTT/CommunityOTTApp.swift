2e4dc6bdff9cdbe97bad8b72f155771d0ad529ec
Hardik Gupta <hardiikgupta12@gmail.com>
2026-08-08 09:43:11 +0530

    Initial Commit

---
+++ CommunityOTT/CommunityOTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityOTTApp.swift
+//  CommunityOTT
+//
+//  Created by Hardik on 08/08/26.
+//
+
+import SwiftUI
+
+@main
+struct CommunityOTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
